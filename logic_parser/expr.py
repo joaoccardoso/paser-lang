e09daf2a5fd493a426d3b303c280ba324e51d056
joaoccardoso <joao.cardoso@fugro.com>
2025-07-05 12:01:21 -0300

    refactoring

---
+++ logic_parser/expr.py
@@ -0,0 +1,60 @@
+import abc
+from dataclasses import dataclass
+from typing import Any
+
+from logic_parser.token import TokenType
+
+
+class Expr(abc.ABC):
+    @abc.abstractmethod
+    def eval(self): ...
+
+
+@dataclass
+class LiteralExpr(Expr):
+    value: Any
+
+    def eval(self):
+        return self.value
+
+
+@dataclass
+class UnaryExpr(Expr):
+    operator: TokenType
+    operand: Expr
+
+    def eval(self):
+        match self.operator:
+            case TokenType.NOT:
+                return not self.operand.eval()
+            case _:
+                raise NotImplementedError(f"Operator {self.operator} not implemented.")
+
+
+@dataclass
+class BinaryExpr(Expr):
+    operator: TokenType
+    r_operand: Expr
+    l_operand: Expr
+
+    def eval(self):
+        A = self.r_operand.eval()
+        B = self.l_operand.eval()
+        match self.operator:
+            case TokenType.BICONDITIONAL:
+                return (A and B) or (not A and not B)
+            case TokenType.IMPLICATION:
+                return (not A) or B
+            case TokenType.OR:
+                return A or B
+            case TokenType.XOR:
+                return (A or B) and not (A and B)
+            case TokenType.AND:
+                return A and B
+            case _:
+                raise NotImplementedError(f"Operator {self.operator} not implemented.")
+
+
+class NoneExpr(Expr):
+    def eval(self):
+        return None
--- logic_parser/parser.py
+++ logic_parser/parser.py
@@ -1,62 +1,6 @@
-import abc
-from dataclasses import dataclass
-from typing import Any
+from typing import Any, Callable
+from logic_parser.expr import BinaryExpr, Expr, LiteralExpr, NoneExpr, UnaryExpr
 from logic_parser.token import Token, TokenType
-
-
-class Expr(abc.ABC):
-    @abc.abstractmethod
-    def eval(self): ...
-
-
-@dataclass
-class LiteralExpr(Expr):
-    value: Any
-
-    def eval(self):
-        return self.value
-
-
-@dataclass
-class UnaryExpr(Expr):
-    operator: TokenType
-    operand: Expr
-
-    def eval(self):
-        match self.operator:
-            case TokenType.NOT:
-                return not self.operand.eval()
-            case _:
-                raise NotImplementedError(f"Operator {self.operator} not implemented.")
-
-
-@dataclass
-class BinaryExpr(Expr):
-    operator: TokenType
-    r_operand: Expr
-    l_operand: Expr
-
-    def eval(self):
-        A = self.r_operand.eval()
-        B = self.l_operand.eval()
-        match self.operator:
-            case TokenType.BICONDITIONAL:
-                return (A and B) or (not A and not B)
-            case TokenType.IMPLICATION:
-                return (not A) or B
-            case TokenType.OR:
-                return A or B
-            case TokenType.XOR:
-                return (A or B) and not (A and B)
-            case TokenType.AND:
-                return A and B
-            case _:
-                raise NotImplementedError(f"Operator {self.operator} not implemented.")
-
-
-class NoneExpr(Expr):
-    def eval(self):
-        return None
 
 
 class Parser:
@@ -79,48 +23,31 @@ class Parser:
         self.pos += 1
         return t
 
+    def parse_binary_op(self, next_parse: Callable[[], Expr], type: TokenType):
+        node = next_parse()
+        while (t := self.peek()) and t.type == type:
+            self.consume(type)
+            right = next_parse()
+            node = BinaryExpr(type, node, right)
+        return node
+
     def parse(self):
         return self.parse_biconditional()
 
     def parse_biconditional(self):
-        node = self.parse_implication()
-        while (t := self.peek()) and t.type == TokenType.BICONDITIONAL:
-            self.consume(TokenType.BICONDITIONAL)
-            right = self.parse_implication()
-            node = BinaryExpr(TokenType.BICONDITIONAL, node, right)
-        return node
+        return self.parse_binary_op(self.parse_implication, TokenType.BICONDITIONAL)
 
     def parse_implication(self):
-        node = self.parse_or()
-        while (t := self.peek()) and t.type == TokenType.IMPLICATION:
-            self.consume(TokenType.IMPLICATION)
-            right = self.parse_or()
-            node = BinaryExpr(TokenType.IMPLICATION, node, right)
-        return node
+        return self.parse_binary_op(self.parse_or, TokenType.IMPLICATION)
 
     def parse_or(self):
-        node = self.parse_xor()
-        while (t := self.peek()) and t.type == TokenType.OR:
-            self.consume(TokenType.OR)
-            right = self.parse_xor()
-            node = BinaryExpr(TokenType.OR, node, right)
-        return node
+        return self.parse_binary_op(self.parse_xor, TokenType.OR)
 
     def parse_xor(self):
-        node = self.parse_and()
-        while (t := self.peek()) and t.type == TokenType.XOR:
-            self.consume(TokenType.XOR)
-            right = self.parse_and()
-            node = BinaryExpr(TokenType.XOR, node, right)
-        return node
+        return self.parse_binary_op(self.parse_and, TokenType.XOR)
 
     def parse_and(self):
-        node = self.parse_not()
-        while (t := self.peek()) and t.type == TokenType.AND:
-            self.consume(TokenType.AND)
-            right = self.parse_not()
-            node = BinaryExpr(TokenType.AND, node, right)
-        return node
+        return self.parse_binary_op(self.parse_not, TokenType.AND)
 
     def parse_not(self):
         if (t := self.peek()) and t.type == TokenType.NOT:
